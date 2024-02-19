d38fbddf37df7b56f9972991c6edbf238d67e2b9
c-aguilar <47341253+c-aguilar@users.noreply.github.com>
2024-02-19 16:58:41 -0500

    Cleaning config paramaters

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-    SPEECH_KEY:"84f2802258854b47bf5352bc65998b02",
-    SPEECH_REGION:"eastus",
-    SPEECH_LANGUAGE:"es-ES",
-    API_BRIDGE_NLQ2SQL_URL:"http://148.210.68.229:30000/query",
-    DEBUG_MODE:true
+    SPEECH_KEY:"",
+    SPEECH_REGION:"",
+    SPEECH_LANGUAGE:"",
+    API_BRIDGE_NLQ2SQL_URL:"",
+    DEBUG_MODE:false
 };
 
 export default config;
